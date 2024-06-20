a8de4241789be12d175b561a5a6884de74394b73
mohammedelbakkali <120609888+mohammedelbakkali@users.noreply.github.com>
2024-06-20 01:55:13 +0100

    first commit

---
+++ GenareteInterface/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace GenareteInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.comboBoxTables = new System.Windows.Forms.ComboBox();
+            this.comboBoxChildTables = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(677, 221);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(35, 354);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(683, 214);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // comboBoxTables
+            // 
+            this.comboBoxTables.FormattingEnabled = true;
+            this.comboBoxTables.Location = new System.Drawing.Point(455, 25);
+            this.comboBoxTables.Name = "comboBoxTables";
+            this.comboBoxTables.Size = new System.Drawing.Size(257, 24);
+            this.comboBoxTables.TabIndex = 3;
+            this.comboBoxTables.SelectedIndexChanged += new System.EventHandler(this.comboBoxTables_SelectedIndexChanged);
+            // 
+            // comboBoxChildTables
+            // 
+            this.comboBoxChildTables.FormattingEnabled = true;
+            this.comboBoxChildTables.Location = new System.Drawing.Point(461, 312);
+            this.comboBoxChildTables.Name = "comboBoxChildTables";
+            this.comboBoxChildTables.Size = new System.Drawing.Size(257, 24);
+            this.comboBoxChildTables.TabIndex = 4;
+            this.comboBoxChildTables.SelectedIndexChanged += new System.EventHandler(this.comboBoxChildTables_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(790, 65);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(373, 460);
+            this.flowLayoutPanel1.TabIndex = 5;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tables";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Children";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 613);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.comboBoxChildTables);
+            this.Controls.Add(this.comboBoxTables);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox comboBoxTables;
+        private System.Windows.Forms.ComboBox comboBoxChildTables;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GenareteInterface/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace GenareteInterface
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, Control> columnControls; // Dictionnaire pour mapper les noms de colonnes aux contrôles
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTableNames();
+        }
+
+        private void LoadTableNames()
+        {
+            string connectionString = "Server=localhost;Port=5432;Database=data_crud;User Id=postgres;Password=root";
+
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"
+                    SELECT table_name
+                    FROM information_schema.tables
+                    WHERE table_schema = 'public' AND table_type = 'BASE TABLE'";
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string tableName = reader.GetString(0);
+                                comboBoxTables.Items.Add(tableName);
+                            }
+                        }
+                    }
+                }
+
+                if (comboBoxTables.Items.Count > 0)
+                {
+                    comboBoxTables.SelectedIndex = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des noms de tables : " + ex.Message);
+            }
+        }
+
+        private void LoadData(string tableName)
+        {
+            string connectionString = "Server=localhost;Port=5432;Database=data_crud;User Id=postgres;Password=root";
+
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = $"SELECT * FROM {tableName}";
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+                                dataGridView1.DataSource = dataTable;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des données : " + ex.Message);
+            }
+        }
+
+        private void LoadChildData(string tableName)
+        {
+            string connectionString = "Server=localhost;Port=5432;Database=data_crud;User Id=postgres;Password=root";
+
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = $"SELECT * FROM {tableName}";
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                DataTable dataTable = new DataTable();
+                                dataTable.Load(reader);
+                                dataGridView2.DataSource = dataTable;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des données des enfants : " + ex.Message);
+            }
+        }
+
+        private void comboBoxTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedTable = comboBoxTables.SelectedItem.ToString();
+            LoadData(selectedTable);
+
+            List<string> childTables = GetChildTables(selectedTable);
+            comboBoxChildTables.Items.Clear();
+            foreach (string childTable in childTables)
+            {
+                comboBoxChildTables.Items.Add(childTable);
+            }
+
+            if (comboBoxChildTables.Items.Count > 0)
+            {
+                comboBoxChildTables.SelectedIndex = 0; // Sélectionner le premier enfant par défaut
+            }
+            else
+            {
+                dataGridView2.DataSource = null; // S'il n'y a pas de tables enfants, on efface le DataGridView
+            }
+        }
+
+        private void comboBoxChildTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedChildTable = comboBoxChildTables.SelectedItem.ToString();
+            LoadChildData(selectedChildTable);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Vérifiez que l'utilisateur a cliqué sur une cellule
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                // Générer dynamiquement les contrôles pour afficher les données de la ligne sélectionnée
+                GenerateControlsForSelectedRow(row);
+            }
+        }
+        private List<string> GetChildTables(string tableName)
+        {
+            List<string> childTables = new List<string>();
+            string connectionString = "Server=localhost;Port=5432;Database=data_crud;User Id=postgres;Password=root";
+
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"
+                SELECT DISTINCT
+                    tc.table_name AS child_table
+                FROM
+                    information_schema.table_constraints AS tc
+                    JOIN information_schema.key_column_usage AS kcu
+                    ON tc.constraint_name = kcu.constraint_name
+                    AND tc.table_schema = kcu.table_schema
+                    JOIN information_schema.constraint_column_usage AS ccu
+                    ON ccu.constraint_name = tc.constraint_name
+                    AND ccu.table_schema = tc.table_schema
+                WHERE
+                    tc.constraint_type = 'FOREIGN KEY' AND
+                    ccu.table_name = @parentTable";
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("parentTable", tableName);
+
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string childTable = reader.GetString(0);
+                                childTables.Add(childTable);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des tables enfants : " + ex.Message);
+            }
+
+            return childTables;
+        }
+
+
+        private void GenerateControlsForSelectedRow(DataGridViewRow row)
+        {
+            ClearColumnControls();
+
+            columnControls = new Dictionary<string, Control>();
+
+            // Créer les contrôles pour chaque colonne de la ligne sélectionnée
+            foreach (DataGridViewCell cell in row.Cells)
+            {
+                string columnName = dataGridView1.Columns[cell.ColumnIndex].HeaderText;
+
+                // Créer un Label pour le nom de la colonne
+                Label label = new Label();
+                label.Text = columnName + ":";
+                label.AutoSize = true;
+
+                // Créer un TextBox ou un ComboBox pour la valeur de la colonne
+                Control inputControl;
+                if (IsColumnForeignKey(columnName))
+                {
+                    // Si c'est une clé étrangère, charger les valeurs possibles depuis la table référencée
+                    ComboBox comboBox = new ComboBox();
+                    comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+                    LoadForeignKeyValues(columnName, comboBox);
+                    comboBox.SelectedValue = cell.Value;
+                    inputControl = comboBox;
+                }
+                else
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Text = cell.Value.ToString();
+                    inputControl = textBox;
+                }
+
+                // Ajouter les contrôles au formulaire
+                flowLayoutPanel1.Controls.Add(label);
+                flowLayoutPanel1.Controls.Add(inputControl);
+
+                // Mapper le nom de la colonne au contrôle correspondant pour accès ultérieur
+                columnControls[columnName] = inputControl;
+            }
+
+            // Ajouter un bouton pour enregistrer les modifications
+            Button saveButton = new Button();
+            saveButton.Text = "Enregistrer";
+            saveButton.Click += SaveChanges_Click;
+            flowLayoutPanel1.Controls.Add(saveButton);
+        }
+
+        private bool IsColumnForeignKey(string columnName)
+        {
+            // Vous pouvez implémenter la logique pour vérifier si la colonne est une clé étrangère
+            // Par exemple, en interrogeant les métadonnées de la base de données
+            return false; // Pour l'exemple, renvoyer toujours faux
+        }
+
+        private void LoadForeignKeyValues(string columnName, ComboBox comboBox)
+        {
+            // Implémentez le chargement des valeurs possibles pour une clé étrangère depuis la base de données
+            // Utilisez columnName pour déterminer la table de référence et chargez les valeurs dans comboBox
+            // Exemple simplifié : charger toutes les valeurs distinctes de la colonne de référence
+            comboBox.Items.Add("Valeur1");
+            comboBox.Items.Add("Valeur2");
+            comboBox.Items.Add("Valeur3");
+        }
+
+        private void SaveChanges_Click(object sender, EventArgs e)
+        {
+            // Enregistrer les modifications de la ligne sélectionnée dans la base de données
+            // Récupérer les nouvelles valeurs depuis les contrôles et exécuter la mise à jour dans la base de données
+
+            // Exemple : récupérer les nouvelles valeurs
+            foreach (var kvp in columnControls)
+            {
+                string columnName = kvp.Key;
+                Control control = kvp.Value;
+
+                if (control is TextBox textBox)
+                {
+                    // Exemple : mise à jour d'une colonne de type texte
+                    string newValue = textBox.Text;
+                    // Implémentez la logique pour mettre à jour la colonne dans la base de données
+                }
+                else if (control is ComboBox comboBox)
+                {
+                    // Exemple : mise à jour d'une colonne avec ComboBox (clé étrangère)
+                    string newValue = comboBox.SelectedItem.ToString();
+                    // Implémentez la logique pour mettre à jour la colonne dans la base de données
+                }
+            }
+
+            MessageBox.Show("Modifications enregistrées avec succès !");
+        }
+
+        private void ClearColumnControls()
+        {
+            // Effacer tous les contrôles d'entrée dans flowLayoutPanel1
+            flowLayoutPanel1.Controls.Clear();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Gérer les clics sur dataGridView2 si nécessaire
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
